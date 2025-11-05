cb9d7b753e31303904bbedc78a0fd16d3814a11c
essafi.wijdane49@gmail.com <wijdanee76@gmail.com>
2025-11-05 11:51:49 +0100

    Fix query_samples.py: use Library.objects.get and remove duplicates

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -14,10 +14,11 @@ book1 = Book.objects.create(title="Harry Potter", author=author1)
 book2 = Book.objects.create(title="Harry Potter 2", author=author1)
 book3 = Book.objects.create(title="Game of Thrones", author=author2)
 
-library1 = Library.objects.create(name="Central Library")
-library1.books.set([book1, book3])
+library = Library.objects.create(name="Central Library")
+library.books.set([book1, book3])
 
-librarian1 = Librarian.objects.create(name="Alice", library=library1)
+librarian = Librarian.objects.create(name="Alice", library=library)
+
 
 
 
@@ -25,9 +26,11 @@ jk_books = Book.objects.filter(author__name="J.K. Rowling")
 print("Books by J.K. Rowling:", [book.title for book in jk_books])
 
 
-library_books = library1.books.all()
-print("Books in Central Library:", [book.title for book in library_books])
+library_name = "Central Library"
+library = Library.objects.get(name=library_name)
+library_books = library.books.all()
+print(f"Books in {library_name}:", [book.title for book in library_books])
 
 
-library_librarian = Librarian.objects.get(library=library1)
+library_librarian = Librarian.objects.get(library=library)
 print("Librarian of Central Library:", library_librarian.name)
