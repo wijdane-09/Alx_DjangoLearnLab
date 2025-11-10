8ffd8fecdc2f00107da4b7101d66cd752853a18b
essafi.wijdane49@gmail.com <wijdanee76@gmail.com>
2025-11-10 15:24:21 +0100

    Move relationship_app inside LibraryProject

---
+++ django-models/LibraryProject/relationship_app/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    publication_year = models.IntegerField(null=True, blank=True)  
+
+    def __str__(self):
+        return f"{self.title} by {self.author.name}"
+
+class Library(models.Model):
+    name = models.CharField(max_length=200)
+    books = models.ManyToManyField(Book)
+
+    def __str__(self):
+        return self.name
+
+class Librarian(models.Model):
+    name = models.CharField(max_length=100)
+    library = models.OneToOneField(Library, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.name} ({self.library.name})"
