0f8a84e2cfa9701cad89e0eb200413c61e7dc5f7
essafi.wijdane49@gmail.com <wijdanee76@gmail.com>
2025-11-10 12:30:47 +0100

    Cleaned up __pycache__ and add .gitignore

---
+++ django-models/relationship_app/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import login_view, logout_view, register_view, list_books
+
+urlpatterns = [
+    path('login/', login_view, name='login'),
+    path('logout/', logout_view, name='logout'),
+    path('register/', register_view, name='register'),
+    path('', list_books, name='list_books'),
+]
