3fb666e98cfaad07bedbb63ddfe07ef966306d37
essafi.wijdane49@gmail.com <wijdanee76@gmail.com>
2025-11-10 16:30:27 +0100

    Added user authentication (register, login, logout) views and templates

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,7 +1,17 @@
 from django.urls import path
-from .views import list_books, LibraryDetailView  # ✅ تأكد من وجود list_books هنا
+from django.contrib.auth.views import LoginView, LogoutView
+from . import views  
+
+from .views import list_books, LibraryDetailView
 
 urlpatterns = [
-    path('books/', list_books, name='list_books'),  # ✅ لعرض جميع الكتب
-    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),  # ✅ لعرض مكتبة واحدة
+    path('books/', list_books, name='list_books'),
+    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
+
+    # ------------------------------
+    # Authentication URLs
+    # ------------------------------
+    path('register/', views.register_view, name='register'),  # register function-based view
+    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),  # class-based login
+    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),  # class-based logout
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render ,redirect
+from django.contrib.auth import login  
+from django.contrib.auth.forms import UserCreationForm  
+from django.contrib import messages
 from django.views.generic.detail import DetailView
 from .models import Book, Library  
 from .models import Library
@@ -16,4 +19,16 @@ def list_books(request):
 class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'  
-    context_object_name = 'library'
+    context_object_name = 'library'  
+
+def register_view(request):
+    if request.method == 'POST':
+        form = UserCreationForm()  
+        form = UserCreationForm(request.POST)  
+        if form.is_valid():
+            user = form.save()
+            messages.success(request, 'Account created successfully!')
+            return redirect('login')
+    else:
+        form = UserCreationForm()  
+    return render(request, 'relationship_app/register.html', {'form': form})
