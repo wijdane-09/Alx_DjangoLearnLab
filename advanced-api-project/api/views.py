a5591e2331c9d798ca54f1306eb82f981a14b478
essafi.wijdane49@gmail.com <wijdanee76@gmail.com>
2025-11-24 14:27:18 +0100

    upload

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,7 +1,8 @@
 from rest_framework import generics
 from .models import Book
 from .serializers import BookSerializer
-from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
+
 
 class BookListView(generics.ListAPIView):
     queryset = Book.objects.all()
@@ -25,7 +26,6 @@ class BookUpdateView(generics.UpdateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [IsAuthenticated]
-
 
 class BookDeleteView(generics.DestroyAPIView):
     queryset = Book.objects.all()
