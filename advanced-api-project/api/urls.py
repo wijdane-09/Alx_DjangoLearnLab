534a355fdc052e598a939e6e45a5292912a3e261
essafi.wijdane49@gmail.com <wijdanee76@gmail.com>
2025-11-24 14:08:09 +0100

    result

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -10,7 +10,7 @@ from .views import (
 urlpatterns = [
     path('books/', BookListView.as_view(), name='book-list'),
     path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),
-    path('books/create/', BookCreateView.as_view(), name='book-create'),ف
+    path('books/create/', BookCreateView.as_view(), name='book-create'),
     path('books/update', BookUpdateView.as_view(), name='book-update'),
     path('books/delete', BookDeleteView.as_view(), name='book-delete'),
 ]
