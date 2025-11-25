6e131d858c188f6d014e0774df5dba4f8a0d7cb0
essafi.wijdane49@gmail.com <wijdanee76@gmail.com>
2025-11-25 12:58:10 +0100

    upload

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,3 +1,4 @@
+from django_filters import rest_framework
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from rest_framework.filters import SearchFilter, OrderingFilter
