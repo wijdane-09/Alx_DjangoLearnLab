5c5b2a322bfd3e4c6936129e6ce01a50cea4be5f
essafi.wijdane49@gmail.com <wijdanee76@gmail.com>
2025-12-02 10:52:52 +0100

    upload

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,7 +1,66 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import logout as django_logout
+from .forms import RegisterForm, ProfileUpdateForm
 
-# Create your views here.
-from django.shortcuts import render
-
+# -------------------
+#    Home Page
+# -------------------
 def home(request):
     return render(request, 'blog/home.html')
+
+
+# -------------------
+#   Register View
+# -------------------
+def register_view(request):
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  # تسجيل الدخول مباشرة بعد التسجيل
+            return redirect("profile")
+    else:
+        form = RegisterForm()
+    return render(request, "auth/register.html", {"form": form})
+
+
+# -------------------
+#   Login View
+# -------------------
+def login_view(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect("profile")
+    else:
+        form = AuthenticationForm()
+    return render(request, "auth/login.html", {"form": form})
+
+
+# -------------------
+#   Logout View
+# -------------------
+def logout_view(request):
+    django_logout(request)
+    return render(request, "auth/logout.html")
+
+
+# -------------------
+#   Profile View
+# -------------------
+@login_required
+def profile_view(request):
+    if request.method == "POST":
+        form = ProfileUpdateForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            return redirect("profile")
+    else:
+        form = ProfileUpdateForm(instance=request.user)
+
+    return render(request, "auth/profile.html", {"form": form})
